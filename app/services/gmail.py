d5958a4cbcee4679896de56ba83e4ebe3b09d986
EricDuong5423 <tuduong05042003@gmail.com>
2025-11-06 21:55:55 +0700

    fix: bug

--- app/services/gmail.py
+++ app/services/gmail.py
@@ -10,8 +10,8 @@ load_dotenv()
 
 conf = ConnectionConfig(
     MAIL_USERNAME = "tuananhtramtinh",
-    MAIL_PASSWORD = str(os.getenv("MAIL_PASSWORD")),
-    MAIL_FROM = str(os.getenv("MAIL_FROM")),
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM = os.getenv("MAIL_FROM"),
     MAIL_PORT = 587,
     MAIL_SERVER = "smtp.gmail.com",
     MAIL_FROM_NAME="Divvy HCMUT",
