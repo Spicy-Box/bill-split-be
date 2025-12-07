14a1c72bbe463c21e4787dc7b114655708146b3b
Nguyễn Tuấn Anh <117497476+tuanemtramtinh@users.noreply.github.com>
2025-12-07 13:06:41 +0700

    return access token value

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -14,7 +14,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
 REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
