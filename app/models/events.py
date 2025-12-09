e65acfeee8af479523bff9b94ac090edf704c04a
EricDuong5423 <tuduong05042003@gmail.com>
2025-12-09 23:01:36 +0700

    Done all events

--- app/models/events.py
+++ app/models/events.py
@@ -5,6 +5,7 @@ from enum import IntEnum
 from beanie import Document, PydanticObjectId
 from pydantic import Field
 from pymongo import IndexModel
+from app.models.bills import Bills
 
 class CurrencyEnum(IntEnum):
     VND = 1
@@ -14,9 +15,11 @@ class CurrencyEnum(IntEnum):
 class Events(Document):
     name: str = Field(..., description="Name of the event")
     currency: CurrencyEnum = Field(..., description="Currency type for the event")
+    description: str = Field(..., description="Description of event")
     participants: list[str] = Field(..., description="List of participant name")
     total_amount: float = Field(..., ge=0, description="Total amount of money for the event")
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    bills: list[Bills] = Field(default_factory=list, description="Bills in event")
 
     class Settings:
         name = "events"
